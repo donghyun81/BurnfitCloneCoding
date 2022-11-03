63762ae5468c3ff5ae7d53c386a6459096c3ea1a
윤동현 <donghyunyoon81@gmail.com>
2022-11-03 15:15:08 +0900

    feat: create bottomNavigation main activity에서 home,calendar,library,mypage등 여러 프래그먼트로 화면을 전환하기 위해서 생성

---
+++ app/src/main/java/com/example/burnfitclonecoding/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.burnfitclonecoding.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.burnfitclonecoding.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
